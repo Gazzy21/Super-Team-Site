712845f68c59f060abccc03372336eab8a9fbf1b
Gazzy21 <nicholas221@icloud.com>
2024-05-06 13:45:10 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Team Member Data
+
+const teamMembers = [
+  {
+    name: "Lionel Messi",
+    age: 36,
+    activePlayer: true,
+    position: "forward",
+    strengths: "Ball Control",
+    weaknesses: "heading ability",
+    skills: ["Dribbling", "Shooting", "Passing"],
+    biography:
+      "Lionel Messi is considered one of the greatest players of all time.",
+  },
+];
+
+function generateTeamCards() {
+  const teamCardsContainer = document.getElementById("teamCards");
+
+  teamMembers.forEach((member) => {
+    const card = document.createElement("div");
+    card.classList.add("col-md-4");
+
+    card.innerHTML = `
+        <div class="card">
+            <div class="card-header">
+            ${member.name}
+            </div>
+        </div>
+        `;
+    teamCardsContainer.appendChild(card);
+  });
+}
+
+window.onload = generateTeamCards();
